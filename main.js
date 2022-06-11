a285ff41522830f28c0f20ea6eee63716a6ad461
Andrew3105 <a.r.n_by@mail.ru>
2022-06-11 07:59:03 +0300

    4 lessons

--- main.js
+++ main.js
@@ -51,3 +51,25 @@
 // const result = replaceZero(myArr);
 // console.log (result);
 
+
+/// Задание 3
+
+const numberOfFilms = +prompt("Сколько фильмов вы посмотрели", '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов', ''),
+    b= prompt("На сколько его оцените", ''),
+    c = prompt('Один из последних просмотренных фильмов', ''),
+    d= prompt("На сколько его оцените", '');
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+    console.log(personalMovieDB);
