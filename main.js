b76fdfa200dab988ce5b10ef9d8eadba24f77cd6
Andrew3105 <a.r.n_by@mail.ru>
2022-06-11 07:27:57 +0300

    examination

--- main.js
+++ main.js
@@ -37,16 +37,17 @@
 
 //2 ПРИМЕР С ЗАМЕНОЙ В МАССИВЕ ЭЛЕМЕНТОВ
 
-const myArr = [12, 53, 20, 18, 22, 200, 43, 57, 50, 1];
-const replaceZero = arr => {
-    for(let i = 0; i < arr.length; i++){
-        const item = arr[i];
-        if (!(item % 10)){
-        const reg = /0/g;
-        arr[i] = item.toString().replace(reg,'zero');
-        }
-    }
-    return arr;
-};
-const result = replaceZero(myArr);
-console.log (result);
+// const myArr = [12, 53, 20, 18, 22, 200, 43, 57, 50, 1];
+// const replaceZero = arr => {
+//     for(let i = 0; i < arr.length; i++){
+//         const item = arr[i];
+//         if (!(item % 10)){
+//         const reg = /0/g;
+//         arr[i] = item.toString().replace(reg,'zero');
+//         }
+//     }
+//     return arr;
+// };
+// const result = replaceZero(myArr);
+// console.log (result);
+
